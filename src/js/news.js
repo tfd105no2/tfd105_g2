2be43d3b0c01e2e98d54ac8884eff82a9c8db3fc
kashinya <tim999ya@gmail.com>
2022-03-23 00:13:04 +0800

    add bgc anime and tab js to news page

--- src/js/news.js
+++ src/js/news.js
@@ -3,19 +3,19 @@
 $(function () {
 
     // tab切換
-    let info_item = $('main.info_box > .info_list .info_item');
-    let info_content = $('main.info_box div.info_content');
+    let info_item = $('#news main.news_box > .tab .tab_item');
+    let info_content = $('main.news_box div.news_content');
     info_content.hide();
     info_content.eq(0).show();
     info_item.eq(0).addClass('js-i-active');
     info_item.on('click', function () {
-
+       
         info_item.removeClass('js-i-active');
         $(this).addClass('js-i-active');
         info_content.hide();
         let tab_infoid = $(this).data('tab');
         $('#' + tab_infoid).show();
+        
     })
 
 });
-
