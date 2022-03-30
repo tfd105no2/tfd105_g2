99b74edef8b2f472804c242f0917c8bc9da5f74f
eUunice <qm54000@gmail.com>
2022-03-30 17:50:17 +0800

    滾輪會滾

--- src/js/traffic.js
+++ src/js/traffic.js
@@ -1,43 +1,67 @@
- // let scroll_to=document.querySelectorAll(".scroll_to");
-    // // console.log(scroll_to);
-    // scroll_to.forEach((item)=>{
-    //   item.addEventListener("click", () =>{
-    //     let alllink=document.getElementById(item.getAttribute("data-id"));
+//  let scroll_to=document.querySelectorAll(".scroll_to");
+//     // console.log(scroll_to);
+//     scroll_to.forEach((item)=>{
+//       item.addEventListener("click", () =>{
+//         let alllink=document.getElementById(item.getAttribute("data-id"));
 
-    //     let linkHeight=alllink.offsetHeight;
-    //     console.log(linkHeight);
+//         let linkHeight=alllink.offsetHeight;
+//         console.log(linkHeight);
         
-    //     alllink.scrollIntoView({behavior:"smooth", block:"start"})
+//         alllink.scrollIntoView({behavior:"smooth", block:"start"})
 
         
-    //   })
+//       })
 
 
-    // })
+//     })
+
+    
 
     let scroll_to=document.querySelectorAll(".scroll_to");
-    // console.log(scroll_to);
+    console.log(scroll_to[0].offsetTop);
     scroll_to.forEach((item)=>{
       item.addEventListener("click", () =>{
-        let alllink=document.getElementById(item.getAttribute("data-id"));
-
-        let linkHeight=alllink.offsetHeight;
-        console.log(linkHeight);
+        let target_top=document.getElementById(item.getAttribute("data-id"));
+        const offsetTop = target_top.getBoundingClientRect().top + window.scrollY - target_top.clientHeight - 10
+        window.scroll({
+          top: offsetTop,
+          behavior: "smooth"
+      });
 
       })
     })
 
-
     // let train_dom = document.getElementsByClassName('fa-train')
-    // // console.log(train_dom[0])
     // train_dom[0].addEventListener('click', ()=>{
-    //   let target_top = document.getElementById("link1")
-    //   console.log(target_top);
-    //   let linkHeight=document.body.offsetHeight;
-    //   console.log(train_dom[0].offsetTop);
-    //   let link = train_dom[0].offsetTop 
+    //   let target_top = document.getElementById("link1");
+    //   const offsetTop = target_top.getBoundingClientRect().top + window.scrollY - target_top.clientHeight - 10
+    //   window.scrollTo({
+    //       top: offsetTop,
+    //       behavior: "smooth"
+    //   });
 
 
+      
+    // });
+
+    // let train_dom2 = document.getElementsByClassName('fa-car')
+    // train_dom2[0].addEventListener('click', ()=>{
+    //   let target_top2 = document.getElementById("link2");
+    //   const offsetTop = target_top2.getBoundingClientRect().top + window.scrollY - target_top2.clientHeight - 10
+    //   window.scrollTo({
+    //       top: offsetTop,
+    //       behavior: "smooth"
+    //   });
+    // });
+
+    // let train_dom3 = document.getElementsByClassName('fa-bus')
+    // train_dom3[0].addEventListener('click', ()=>{
+    //   let target_top3 = document.getElementById("link3");
+    //   const offsetTop = target_top3.getBoundingClientRect().top + window.scrollY - target_top3.clientHeight - 10
+    //   window.scrollTo({
+    //       top: offsetTop,
+    //       behavior: "smooth"
+    //   });
     // });
 
 
