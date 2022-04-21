2a437c0d44ff9d9573ed8246ab8b4e98dd75d877
eUunice <qm54000@gmail.com>
2022-04-21 12:08:52 +0800

    add creditcard  popup

--- src/js/checkout_pay.js
+++ src/js/checkout_pay.js
@@ -1,9 +1,18 @@
+Vue.use(VueMask.VueMaskPlugin);
 new Vue({
     el: '#root',
     data: {
         products: [],
         score: 100,
-        payway: 'creditcard',
+        payway:'',
+        creditcard:'creditcard',
+        linepay:'linepay',
+        current_edit: false,
+        name: '',
+        cardid: '',
+        carddate: '',
+        cardsafe: '',
+        
     },
     created() {
         let tasks = JSON.parse(localStorage.getItem("ticketsData"));
@@ -19,8 +28,67 @@ new Vue({
             let num = sum - this.score;
             return num;
         },
-        post() {
 
+        toggle() {
+            if(this.payway === this.creditcard){
+                this.current_edit = true;
+            }else if(this.payway === ''){
+
+               return
+               
+            }else{
+                this.post();
+
+            }
+        },
+
+        edit(index) {
+            this.current_edit = index;
+            
+        },
+        f_out() {
+            this.current_edit = null;
+            
+        },
+        confirmCard() {
+          let val = /^[\u4E00-\u9FA5]+$/;
+          let dateObj = this.carddate.split('/');
+          let cardIdobj = this.cardid.length;
+          this.cardid.innerHTML = cardIdobj;
+          let cardSafeobj = this.cardsafe.length;
+          this.cardsafe.innerHTML = cardSafeobj;
+
+          if (val.test(this.name) == false) {
+            alert('請輸入中文姓名');
+            this.name = '';
+            this.cardid = '';
+            this.carddate = '';
+            this.cardsafe = '';
+          } else if (cardIdobj < 19) {
+            alert('請輸入正確的信用卡號');
+            this.name = '';
+            this.cardid = '';
+            this.carddate = '';
+            this.cardsafe = '';
+          } else if (dateObj[0] > 12 || dateObj[0] < 1 || dateObj[1] < 21 || dateObj[1] > 35) {
+            alert('請輸入正確的有效日期');
+            this.name = '';
+            this.cardid = '';
+            this.carddate = '';
+            this.cardsafe = '';
+          } else if (cardSafeobj != 3) {
+            alert('請輸入正確安全碼');
+            this.name = '';
+            this.cardid = '';
+            this.carddate = '';
+            this.cardsafe = '';
+          } else {
+            location.href = "checkout_complet.html";
+          }
+        },
+
+
+        post() {
             let order_id = Date.now().toString().slice(-6);
             axios.post("../php/order.php",
                 {
